31ae94edecc3b7a679de8f47a763ca662279ae30
HackinTushar <HackinTushar@tushars-MacBook-Air.local>
2022-12-18 12:29:10 +0530

    first commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by HackinTushar on 18/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
